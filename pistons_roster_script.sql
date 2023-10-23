8bbdd88981d2aa667e697af16851ed7c32dcebc2
Andrew Tran <tranandrew@gmail.com>
2023-10-23 15:18:11 -0500

    added pistons_roster_script.sql to file. Use to create table using mysqlworkbench

---
+++ pistons_roster_script.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE  IF NOT EXISTS `pistons_roster`;
+USE `pistons_roster`;
+
+--
+-- table structure for table 'roster'
+
+DROP TABLE IF EXISTS `roster`;
+
+CREATE TABLE `roster`(
+	`number` int NOT NULL,
+    `first_name` VARCHAR(45) DEFAULT NULL,
+    `last_name` VARCHAR(45) DEFAULT NULL,
+    `position` VARCHAR(15) DEFAULT NULL,
+    PRIMARY KEY (`number`)
+);
+
+--
+-- Data for table 'roster'
+--
+
+INSERT INTO `roster` VALUES
+	(2, 'Cade', 'Cunningham', 'PG'),
+    (35, 'Marvin', 'Bagley III', 'PF'),
+    (27, 'Buddy', 'Boeheim', 'F'),
+    (44, 'Bojan', 'Bogdanovic', 'SF'),
+    (14, 'Alec', 'Burks', 'SG'),
+    (20, 'Malcolm', 'Cazalon', 'G'),
+    (0, 'Jalen', 'Duren', 'C'),
+    (31,'Joe','Harris','SF'),
+    (7,'Killian','Hayes','PG'),
+    (23,'Jaden','Ivey','G'),
+    (12,'Isaiah','Livers','PF'),
+    (5,'Monte','Morris','PG'),
+    (4,'Jontay','Porter','C'),
+    (8,'Jared','Rhoden','G'),
+    (28,'Isaiah','Stewart','C'),
+    (9,'Ausar','Thompson','F'),
+    (13,'James','Wiseman','C');
+    
