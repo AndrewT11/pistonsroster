cd0b05436d656b1ca63ffdf08a2a4097e63affba
Andrew Tran <tranandrew@gmail.com>
2023-10-23 09:13:48 -0500

    no args constructor, constructor, @Override toString()

--- src/main/java/com/personal/pistonsroster/entity/Player.java
+++ src/main/java/com/personal/pistonsroster/entity/Player.java
@@ -19,4 +19,60 @@ public class Player {
     @Column(name="position")
     private String position;
 
+    // no args constructor
+
+    public Player() {}
+
+    // args constructor
+
+    public Player(int id, String firstName, String lastName, String position) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    }
+
+    // getters / setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
 }
