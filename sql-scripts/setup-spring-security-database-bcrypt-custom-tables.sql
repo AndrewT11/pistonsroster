ad0e6806987235a88ae2750b37d6677639c9ce2f
Andrew Tran <tranandrew@gmail.com>
2023-11-02 20:21:54 -0500

    create sql-script folder. create setup-spring-security-database-bcrypt-custom-tables for member table, bcrypt password, authorities table, and roles for each user

---
+++ sql-scripts/setup-spring-security-database-bcrypt-custom-tables.sql
@@ -0,0 +1,54 @@
+USE `pistons_roster`;
+
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `members`;
+
+--
+-- Table structure for table 'members
+--
+
+CREATE TABLE `members` (
+    `user_id` varchar(50) NOT NULL,
+    `pw` char(68) NOT NULL,
+    `active` tinyint NOT NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table 'members'
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- Generation tool for encrypted passwords: https://www.bcryptcalculator.com/
+--
+-- Default passwords for all users: testing123
+
+INSERT INTO `members`
+VALUES
+('andrew'`,'{bcrypt}$2a$10$x0jSk1oMB5lR25WhfcUF7ePEnfxNS500i2nwiGvUKWJ1cdHDGLO0C')
+('greg','{bcrypt}$2a$10$x0jSk1oMB5lR25WhfcUF7ePEnfxNS500i2nwiGvUKWJ1cdHDGLO0C')
+('monique','{bcrypt}$2a$10$x0jSk1oMB5lR25WhfcUF7ePEnfxNS500i2nwiGvUKWJ1cdHDGLO0C')
+
+--
+-- Table structure for table 'authorities'
+--
+
+CREATE TABLE  `roles` (
+    `user_id` varchar(50) NOT NULL,
+    `role` varchar(50) NOT NULL,
+    UNIQUE KEY `authorities5_idx_1` (`user_id`, `role`),
+    CONSTRAINT `authorities5_ibfk_1` FOREIGN KEY(`user_id`) REFERENCES `members` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `roles`
+--
+
+INSERT INTO `roles`
+VALUES
+('andrew','ROLE_EMPLOYEE')
+('andrew','ROLE_MANAGER')
+('andrew','ROLE_ADMIN')
+('greg','ROLE_EMPLOYEE')
+('greg','ROLE_MANAGER')
+('monique','ROLE_EMPLOYEE')
