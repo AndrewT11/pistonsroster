d906ada07bed3c7256e97a068dc5022a33c0fbbb
Andrew Tran <tranandrew@gmail.com>
2023-11-02 20:49:15 -0500

    filterChain() method to restrict certain endpoints to user roles/authorities

--- src/main/java/com/personal/pistonsroster/security/SecurityConfig.java
+++ src/main/java/com/personal/pistonsroster/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.personal.pistonsroster.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -26,5 +30,25 @@ public class SecurityConfig {
         );
 
         return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/players").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/players/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/players").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/players").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/players/**").hasRole("ADMIN")
+        );
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        // is general, not required for stateless REST APIs that use POST, PUT, DELETE, and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
